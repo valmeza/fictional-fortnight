6c06856a60e6a9d32c45895e8094ef5ab8bc8270
Valeria Meza <valeriameza173@gmail.com>
2020-12-15 15:23:23 -0600

    created a really simple counter using onClick and useState method.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CounterExample from './components/CounterExample';
 import HelloWorld from './components/HelloWorld';
 
 // this is a react component
@@ -9,7 +10,8 @@ function App() {
     // This funny tag syntax is neither a string nor HTML.
     // It is called JSX, and it is a syntax extension to JavaScript.
     <div>
-      <HelloWorld name="Valeria"/> 
+      {/* <HelloWorld name="Valeria"/>  */}
+      <CounterExample/>
     </div>
     // JSX elements must be wrapped in an enclosing tag
     // you cannot have two if you want to add more elements they must be
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 // stateless/functional component
-
 function HelloWorld(prop) {
     return (
         <h1>Hello {prop.name}</h1>
     )
 }
+
+
+// class component
+// class HelloWorld extends React.Component {
+//     render() {
+//         return (
+//             <h1>Hello {this.props.name}!</h1> // for class components use this.props.name
+//         )
+//     }
+// }
 
 export default HelloWorld;
