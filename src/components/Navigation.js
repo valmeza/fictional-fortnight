bf1680fcd63f22f9c5a83e01f0cb8c4853917d82
Valeria Meza <valeriameza173@gmail.com>
2020-12-15 23:18:05 -0600

    add custom tailwind color and add even listener to close menu.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,10 +9,16 @@ function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
 
     let menu; // initialize variable but don't assign until condition is met
+    let menuMask;
 
     if (showMenu) { // if true then render this
         menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
             This is the menu
+            </div>
+        menuMask = <div
+            onClick={() => setShowMenu(false)}
+            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50">
+
             </div>
     }
     
@@ -24,6 +30,8 @@ function Navigation() {
                     onClick={() => setShowMenu(!showMenu)} // toggle between true or false
                 />
             </span>
+
+            { menuMask }
 
             { menu }
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'black-t-50': 'rgba(0,0,0,0.5)',
+      }
+    },
   },
   variants: {
     extend: {},
