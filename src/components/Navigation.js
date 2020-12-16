b8a632606c0d627fc3f8ad5a12c58c6bef68953d
Valeria Meza <valeriameza173@gmail.com>
2020-12-16 16:39:38 -0600

    add animation to side menu using react-spring library

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,24 @@
 import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import {useTransition, animated} from 'react-spring'
 
 function Navigation() {
     // state/ react hook
     // variable showMenu, method setShowMenu
     // hook useState
     const [showMenu, setShowMenu] = useState(false)
+    const maskTransitions = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
 
-    let menu; // initialize variable but don't assign until condition is met
-    let menuMask;
-
-    if (showMenu) { // if true then render this
-        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-            This is the menu
-            </div>
-        menuMask = <div
-            onClick={() => setShowMenu(false)}
-            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50">
-
-            </div>
-    }
+    const menuTransitions = useTransition(showMenu, null, {
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
+    })
     
     return (
         <nav>
@@ -31,9 +29,40 @@ function Navigation() {
                 />
             </span>
 
-            { menuMask }
+            {
+                // black background / mask
+                maskTransitions.map(({ item, key, props }) =>
+                    item && <animated.div
+                        key={key}
+                        style={props}
+                        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+                        onClick={() => setShowMenu(false)}
+                    >
+                        
+                    </animated.div>
+                )
+            }
 
-            { menu }
+            {
+                // side menu
+                menuTransitions.map(({ item, key, props }) =>
+                    item && <animated.div
+                        key={key}
+                        style={props}
+                        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+                    >
+                        <span className="font-bold">
+                            The Menu
+                        </span>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </animated.div>
+                )
+            }
+
         </nav>
     )
 }
